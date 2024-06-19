e04f596710a4c600b80bb8b7ab379c2b87c9f873
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-19 23:19:56 +0300

    Lecture 76 - Yorum Silme

---
+++ SeriesBlogSite/AdminPages/AdminDeleteComment.aspx.cs
@@ -0,0 +1,20 @@
+﻿using SeriesBlogSite.Entity;
+using System;
+
+namespace SeriesBlogSite.AdminPages
+{
+    public partial class AdminDeleteComment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["Delete"]);
+            using (SeriesBlogSiteEntities db = new SeriesBlogSiteEntities())
+            {
+                var value = db.tbl_comment.Find(id);
+                db.tbl_comment.Remove(value);
+                db.SaveChanges();
+                Response.Redirect("/AdminPages/AdminComments.aspx");
+            }
+        }
+    }
+}
