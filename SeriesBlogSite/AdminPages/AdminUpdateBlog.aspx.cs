135eb5d6511494b6fd80fd047c58611e8ae369a3
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-19 22:19:46 +0300

    Lecture 72 - Güncellenecek Bloğun Bilgilerini Taşıma

--- SeriesBlogSite/AdminPages/AdminUpdateBlog.aspx.cs
+++ SeriesBlogSite/AdminPages/AdminUpdateBlog.aspx.cs
@@ -9,6 +9,7 @@ namespace SeriesBlogSite.AdminPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            int id =int.Parse(Request.QueryString["Update"]);
             if (!IsPostBack)
             {
                 using (SeriesBlogSiteEntities db = new SeriesBlogSiteEntities())
@@ -24,6 +25,13 @@ namespace SeriesBlogSite.AdminPages
                     ddlBlgCategory.DataTextField = "ctg_name";
                     ddlBlgCategory.DataValueField = "ctg_id";
                     ddlBlgCategory.DataBind();
+
+                    var blog = db.tbl_blog.Find(id);
+                    txtBlgTitle.Text = blog.blg_title;
+                    txtBlgContent.Text = blog.blg_contents;
+                    txtBlgImage.Text = blog.blg_picture;
+                    ddlBlgType.SelectedValue = blog.blg_type.ToString();
+                    ddlBlgCategory.SelectedValue = blog.blg_category.ToString();
 
                 }
             }
